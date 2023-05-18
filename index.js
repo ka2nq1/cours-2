522802f39cf82f1710d9b74ab9bc1b9fc5a4c413
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-05-18 16:19:28 +0300

    cmt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const resultsItem = document.getElementsByClassName('programAcordeonItem');
 
 for (let i = 0; i < resultsItem.length; i++) {
-    resultsItem[i].addEventListener("click", function() {
+    resultsItem[i].addEventListener("click", function () {
         for (let j = 0; j < resultsItem.length; j++) {
             if (i !== j) {
                 resultsItem[j].classList.remove("active");
@@ -14,7 +14,7 @@ for (let i = 0; i < resultsItem.length; i++) {
 const questionsItem = document.getElementsByClassName('questionsItem');
 
 for (let i = 0; i < questionsItem.length; i++) {
-    questionsItem[i].addEventListener("click", function() {
+    questionsItem[i].addEventListener("click", function () {
         for (let j = 0; j < questionsItem.length; j++) {
             if (i !== j) {
                 questionsItem[j].classList.remove("active");
@@ -24,3 +24,15 @@ for (let i = 0; i < questionsItem.length; i++) {
     });
 }
 
+const overlay = document.getElementById('overlay');
+const videoIframe = document.getElementById('video-iframe');
+function openOverlay(src) {
+
+    videoIframe.src = src;
+    overlay.style.display = 'block';
+}
+
+function closeOverlay() {
+    overlay.style.display = 'none';
+    videoIframe.src = '';
+}
